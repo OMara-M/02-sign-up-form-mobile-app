4f4c700b00d2d93d1e43c7fcd0183d9ca4c00c45
Matthew O'Mara <tuh39725@temple.edu>
2022-02-02 21:27:38 -0500

    refactor save button name

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,15 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button in the form
+        findViewById<Button>(R.id.btnSave)
+
     }
 }
